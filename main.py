c4ac7ef4cd2b79898cc82c7908c7ffcca44595ce
Muhamad Padli Haikal <padlihaikal10@gmail.com>
2026-09-02 21:58:23 +0700

    Add conversation memory, system prompt, and basic streaming to chat endpoint

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ from openai import OpenAI
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
 app = FastAPI()
+history = [
+    {"role": "system", "content": "Kamu adalah asisten yang ramah dan menjawab singkat dalam Bahasa Indonesia."}
+]
 
 class ChatRequest(BaseModel):
     message: str
@@ -20,11 +26,20 @@ class ChatResponse(BaseModel):
 
 @app.post("/chat/", response_model=ChatResponse)
 def chat(request: ChatRequest):
-    result = client.chat.completions.create(model="gpc",
-                                   messages=[{
-                                       "role": "user",
-                                       "content":request.message
-                                       }]
-                                       )
-    reply_text = result.choices[0].message.content
+    global history
+    history.append({"role": "user", "content": request.message})
+    
+    result = client.chat.completions.create(
+        model="openai/gpt-oss-20b",
+            messages=history,
+            stream=True
+            )
+
+    reply_text = ""
+    for chunk in result: 
+        delta = chunk.choices[0].delta.content
+        if delta:
+            reply_text += delta
+    
+    history.append({"role": "assistant", "content": reply_text})
     return ChatResponse(reply=reply_text)
