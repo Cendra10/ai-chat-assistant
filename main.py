eab771a080addb9c999771b93f1e6d4ccdee3aa5
Muhamad Padli Haikal <padlihaikal10@gmail.com>
2026-09-03 22:15:23 +0700

    Add function calling with get_current_time tool

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv 
 from pydantic import BaseModel
 from openai import OpenAI
+from datetime import datetime
 
 load_dotenv()
 
@@ -32,14 +33,52 @@ def chat(request: ChatRequest):
     result = client.chat.completions.create(
         model="openai/gpt-oss-20b",
             messages=history,
-            stream=True
+            tools=tools
             )
-
-    reply_text = ""
-    for chunk in result: 
-        delta = chunk.choices[0].delta.content
-        if delta:
-            reply_text += delta
     
-    history.append({"role": "assistant", "content": reply_text})
-    return ChatResponse(reply=reply_text)
+    message = result.choices[0].message
+    if message.tool_calls:
+        function_name = message.tool_calls[0].function.name
+        print(function_name)
+
+        if function_name == "get_current_time":
+            tool_result = get_current_time()
+
+        history.append({
+            "role": "tool",
+            "tool_call_id": message.tool_calls[0].id,
+            "name": function_name,
+            "content": tool_result
+            })
+        
+        result = client.chat.completions.create(
+            model="openai/gpt-oss-20b",
+            messages=history,
+            tools=tools
+            )
+        reply_text = result.choices[0].message.content
+        history.append({"role": "assistant", "content": reply_text})
+        return ChatResponse(reply=reply_text)
+    else:
+        reply_text = message.content
+        history.append({"role": "assistant", "content": reply_text})
+        return ChatResponse(reply=reply_text)
+
+
+def get_current_time():
+    return datetime.now().strftime("%H:%M:%S")
+
+tools = [
+    {
+        "type": "function",
+        "function":{
+            "name":"get_current_time",
+            "description": "Mengambil waktu saat ini dalam format HH:MM:SS",
+            "parameters":{
+                "type": "object",
+                "properties": {},
+                "required": []
+            }
+        }
+    }
+]
